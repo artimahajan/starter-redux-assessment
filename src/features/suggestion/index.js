27b08a30923fbf052ca3e5ab16d2cc2b0de38e51
“Arati <arti.mahajan86@gmail.com>
2024-05-17 09:58:30 -0700

    React reduct doggigram assessment

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -1,23 +1,26 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   fetchSuggestion,
   selectError,
   selectLoading,
-  // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
-} from './suggestion.slice';
-import './suggestion.css';
+  // √ Task 18: Import the `selectSuggestion()` selector from the suggestion slice
+  selectSuggestion,
+} from "./suggestion.slice";
+import "./suggestion.css";
 
 export default function Suggestion() {
-  // Task 19: Call useSelector() with the selectSuggestion() selector
+  // √ Task 19: Call useSelector() with the selectSuggestion() selector
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const { suggestion } = useSelector(selectSuggestion);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadSuggestion() {
-      // Task 20: Dispatch the fetchSuggestion() action creator
+      // √ Task 20: Dispatch the fetchSuggestion() action creator
+      dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
@@ -28,11 +31,11 @@ export default function Suggestion() {
   } else if (error) {
     render = <h3>Sorry, we're having trouble loading the suggestion.</h3>;
   } else {
-    // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
+    // √ Task 21: Enable the two JSX lines below needed to display the suggestion on the page
     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        <img alt={suggestion.caption} src={suggestion.imageUrl} />
+        <p>{suggestion.imageUrl}</p>
       </>
     );
   }
